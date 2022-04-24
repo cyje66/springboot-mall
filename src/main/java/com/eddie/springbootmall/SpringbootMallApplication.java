220f7adf782302128f7a08571c6f7bc11b6f922c
cyje66 <eddie71677@gmail.com>
2022-04-24 15:26:18 +0800

    init spring project

---
+++ src/main/java/com/eddie/springbootmall/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.eddie.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMallApplication.class, args);
+    }
+
+}
