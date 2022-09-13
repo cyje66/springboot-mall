bd9bf7ddd8dfbbc95e026722f71ca8c388f96175
Eddie Chien <eddie71677@gmail.com>
2022-09-13 15:42:34 +0800

    Implement login functions

--- src/main/java/com/eddie/springbootmall/controller/UserController.java
+++ src/main/java/com/eddie/springbootmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.eddie.springbootmall.controller;
 
+import com.eddie.springbootmall.dto.UserLoginRequest;
 import com.eddie.springbootmall.dto.UserRegisterRequest;
 import com.eddie.springbootmall.model.User;
 import com.eddie.springbootmall.service.UserService;
@@ -25,5 +26,12 @@ public class UserController {
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/eddie/springbootmall/service/UserService.java
+++ src/main/java/com/eddie/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.eddie.springbootmall.service;
 
+import com.eddie.springbootmall.dto.UserLoginRequest;
 import com.eddie.springbootmall.dto.UserRegisterRequest;
 import com.eddie.springbootmall.model.User;
 
@@ -8,4 +9,6 @@ public interface UserService {
     Integer register(UserRegisterRequest userRegisterRequest);
 
     User getUserById(Integer userId);
+
+    User login(UserLoginRequest userLoginRequest);
 }
